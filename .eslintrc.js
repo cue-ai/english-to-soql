7f851c73ae0bdb39adba7fa8bccb80872aceb6f9
Caelin Sutch <caelinsutch@gmail.com>
2023-07-07 10:36:38 -0400

    Fix build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint", "react", "prettier", "unused-imports"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
 };
