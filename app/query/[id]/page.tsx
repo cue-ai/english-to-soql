02e1c07ccfcfdf6ed9f4ccbca10643ef7a69dee5
anirudhramoo <anirudh@getpassage.co>
2023-07-13 17:34:14 -0400

    share stuff done

--- app/query/[id]/page.tsx
+++ app/query/[id]/page.tsx
@@ -2,19 +2,6 @@ import QueryResult from "@/app/query/[id]/QueryResult";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: {
-    default: "Salesforce SOQL Generator",
-    template: `SOQL Converter`,
-  },
-  description:
-    '"Experience the power of AI with our GPT-driven tool, adeptly transforming English natural language into Salesforce SOQL queries. ' +
-    "Streamline your data management process, minimize coding efforts, and maximize productivity. " +
-    "Ideal for beginners and seasoned Salesforce users alike, our tool offers unparalleled ease-of-use and accuracy in generating " +
-    "SOQL queries. " +
-    'Revolutionize your Salesforce experience today, boost efficiency, and unlock deeper insights from your data."',
-  icons: {
-    icon: "/icon.ico",
-  },
   openGraph: {
     title: "Salesforce SOQL Generator",
     description:
