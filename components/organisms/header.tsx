f45842297413dcf38a543d4cf9e2060d36f71e6b
anirudhramoo <anirudh@getpassage.co>
2023-07-14 12:18:19 -0400

    minor change

--- components/organisms/header.tsx
+++ components/organisms/header.tsx
@@ -1,16 +1,21 @@
 "use client";
-export const Header = () => (
-  <header className="absolute  top-0 z-50 flex items-center justify-between w-full h-14 px-24 shrink-0 ">
-    <div className="flex items-center justify-start space-x-2 w-full h-full border-b border-gray-500 border-opacity-50 font-bold text-white mt-10">
-      <div
-        className={"inline-block flex cursor-pointer"}
-        onClick={() => {
-          location.reload();
-        }}
-      >
-        <img src="/salesforceLogo.svg" alt="Logo" className={"mr-2"} />
-        <h1>AskSalesforce</h1>
+
+export const Header = () => {
+  const handleOnClick = () => {
+    window.location.href = process.env.NEXT_PUBLIC_FRONTEND_URL as string;
+  };
+
+  return (
+    <header className="absolute  top-0 z-50 flex items-center justify-between w-full h-14 px-24 shrink-0 ">
+      <div className="flex items-center justify-start space-x-2 w-full h-full border-b border-gray-500 border-opacity-50 font-bold text-white mt-10">
+        <div
+          className={"inline-block flex cursor-pointer"}
+          onClick={handleOnClick}
+        >
+          <img src="/salesforceLogo.svg" alt="Logo" className={"mr-2"} />
+          <h1>AskSalesforce</h1>
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
